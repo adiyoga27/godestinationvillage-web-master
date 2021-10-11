47935058cb6c0eedd165769be997eb398cd2bc3f
Ida Bagus Ketut Adiyoga <adiyoga27new@gmail.com>
2021-10-11 17:48:37 +0800

    update payment

--- resources/views/customer/payment/midtrans.blade.php
+++ resources/views/customer/payment/midtrans.blade.php
@@ -1,24 +1,39 @@
 <html>
 <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
     <script>
+        var redirectUriEvent = 'window.location.href({{url("reservation-events/paid/$order[customer_email]")}})';
+        var redirectUriTour = 'window.location.href({{url("reservation/paid/$order[customer_email]")}})';
+alert('{{$redirectURI}}');
+     
             snap.pay('{{ $snapToken }}', {
                 // Optional
                 onSuccess: function(result) {
                     /* You may add your own js here, this is just example */
                     // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    console.log(result)
+                    console.log(result);    
+                    alert('success');
+
+                    window.location.href = '{{$redirectURI}}';
                 },
                 // Optional
                 onPending: function(result) {
                     /* You may add your own js here, this is just example */
                     // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    console.log(result)
+                    alert('pending');
+
+                    window.location.href = '{{$redirectURI}}';
+
+                    console.log(result);
                 },
                 // Optional
                 onError: function(result) {
+                    alert('error');
+
                     /* You may add your own js here, this is just example */
                     // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    console.log(result)
+                    window.location.href = '{{$redirectURI}}' ;
+
+                    console.log(result);
                 }
             });
 
