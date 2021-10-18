d9848820779d10ae403c0b19771cbec68efb426e
Ida Bagus Ketut Adiyoga <adiyoga27new@gmail.com>
2021-10-18 19:07:39 +0800

    update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -26,6 +26,7 @@ class AuthController extends Controller
     {
 
         $user = Socialite::driver($provider)->user();
+    
         $authUser = $this->findOrCreateUser($user, $provider);
         Auth::login($authUser, true);
         return redirect('/');
