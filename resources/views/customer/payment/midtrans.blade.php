ec3bf1dc8dfc53dd036fd12059dae98c822ece5e
Ida Bagus Ketut Adiyoga <adiyoga27new@gmail.com>
2021-10-18 21:40:40 +0800

    uri

--- resources/views/customer/payment/midtrans.blade.php
+++ resources/views/customer/payment/midtrans.blade.php
@@ -1,62 +1,60 @@
 <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-        <head>
-            <meta charset="UTF-8" />
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8" />
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
     <meta name="viewport"
         content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi">
 
-            <meta property="og:site_name" content="Godevi">
-            <meta property="og:title" content="{{ $title ?? 'GODEVI - Authentic Village Experiences' }}" />
-            <meta property="og:description"
-                content="{{ $content ?? 'Automatic Approve Payment' }}" />
-            <meta property="og:image" itemprop="image" content="{{ $image ?? url('frontdata/images/bird.png') }}">
-            <meta property="og:type" content="website" />
-            <meta property="og:updated_time" content="1440432930" />
+    <meta property="og:site_name" content="Godevi">
+    <meta property="og:title" content="{{ $title ?? 'GODEVI - Authentic Village Experiences' }}" />
+    <meta property="og:description" content="{{ $content ?? 'Automatic Approve Payment' }}" />
+    <meta property="og:image" itemprop="image" content="{{ $image ?? url('frontdata/images/bird.png') }}">
+    <meta property="og:type" content="website" />
+    <meta property="og:updated_time" content="1440432930" />
 
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-            <!-- CSRF Token -->
-        
-            <title>{{ config('app.name', 'Godesination Village') }}</title>
-            <link rel="icon" href="{{ url('customer/img/favicon.png') }}" type="image/png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        
-        </head>
-    <body>
-<script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
-    <script>
-  
-            snap.pay('{{ $snapToken }}', {
-                // Optional
-                onSuccess: function(result) {
-                    /* You may add your own js here, this is just example */
-                    // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    console.log(result);    
+    <title>{{ config('app.name', 'Godesination Village') }}</title>
+    <link rel="icon" href="{{ url('customer/img/favicon.png') }}" type="image/png" />
 
-                    window.location.href = '{{$redirectURISuccess}}';
-                },
-                // Optional
-                onPending: function(result) {
-                    /* You may add your own js here, this is just example */
-                    // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
 
-                    window.location.href = '{{$redirectURIError}}';
+</head>
 
-                    console.log(result);
-                },
-                // Optional
-                onError: function(result) {
-
-                    /* You may add your own js here, this is just example */
-                    // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    window.location.href = '{{$redirectURIError}}' ;
-
-                    console.log(result);
-                }
-            });
-
-       
+<body>
+    <script src="{{env('MIDTRANS_URI_SANDBOX')}}" data-client-key="{{ config('midtrans.client_key') }}">
     </script>
-       </body>
-       </html>
+    <script>
+        snap.pay('{{ $snapToken }}', {
+            // Optional
+            onSuccess: function(result) {
+                /* You may add your own js here, this is just example */
+                // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+                console.log(result);
+
+                window.location.href = '{{ $redirectURISuccess }}';
+            },
+            // Optional
+            onPending: function(result) {
+                /* You may add your own js here, this is just example */
+                // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+
+                window.location.href = '{{ $redirectURIError }}';
+
+                console.log(result);
+            },
+            // Optional
+            onError: function(result) {
+
+                /* You may add your own js here, this is just example */
+                // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+                window.location.href = '{{ $redirectURIError }}';
+
+                console.log(result);
+            }
+        });
+    </script>
+</body>
+
+</html>
