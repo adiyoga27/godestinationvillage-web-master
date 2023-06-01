b6383ce9a2dab11b53b19457df382c4c9e191e8c
adiyoga <adiyoga27new@gmail.com>
2023-06-02 02:11:19 +0800

    add chat

--- resources/views/customer/layout.blade.php
+++ resources/views/customer/layout.blade.php
@@ -689,28 +689,30 @@ $( "form" ).submit(function( event ) {
         //         $(this).parent().addClass('active')
         //     }).filter(':first').click();
         // });
-        (function() {
-            var options = {
-                facebook: "259094734978868", // Facebook page ID
-                whatsapp: "+6282236803301", // WhatsApp number
-                call_to_action: "Message us", // Call to action
-                button_color: "#FF6550", // Color of button
-                position: "right", // Position may be 'right' or 'left'
-                order: "facebook,whatsapp", // Order of buttons
-            };
-            var proto = document.location.protocol,
-                host = "whatshelp.io",
-                url = proto + "//static." + host;
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = url + '/widget-send-button/js/init.js';
-            s.onload = function() {
-                WhWidgetSendButton.init(host, proto, options);
-            };
-            var x = document.getElementsByTagName('script')[0];
-            x.parentNode.insertBefore(s, x);
-        })();
+        // (function() {
+        //     var options = {
+        //         facebook: "259094734978868", // Facebook page ID
+        //         whatsapp: "+6282236803301", // WhatsApp number
+        //         call_to_action: "Message us", // Call to action
+        //         button_color: "#FF6550", // Color of button
+        //         position: "right", // Position may be 'right' or 'left'
+        //         order: "facebook,whatsapp", // Order of buttons
+        //     };
+        //     var proto = document.location.protocol,
+        //         host = "whatshelp.io",
+        //         url = proto + "//static." + host;
+        //     var s = document.createElement('script');
+        //     s.type = 'text/javascript';
+        //     s.async = true;
+        //     s.src = url + '/widget-send-button/js/init.js';
+        //     s.onload = function() {
+        //         WhWidgetSendButton.init(host, proto, options);
+        //     };
+        //     var x = document.getElementsByTagName('script')[0];
+        //     x.parentNode.insertBefore(s, x);
+        // })();
+
+        <script id="pixel-chaty" async="true" src="https://cdn.chaty.app/pixel.js?id=stx7rWCJ"></script>
 
         $('.hides').hide()
         $('.css').hide()
