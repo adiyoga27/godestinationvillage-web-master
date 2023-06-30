4035f93849a7fde0ae519761357396b8b87d4358
adiyoga <adiyoga27new@gmail.com>
2023-07-01 00:01:48 +0800

    update icon

--- resources/views/customer/founding.blade.php
+++ resources/views/customer/founding.blade.php
@@ -1,6 +1,5 @@
 @extends('customer/layout')
 @section('content')
-
     <!-- start page title area-->
     <div class="page-title-area ptb-100">
         <div class="container">
@@ -28,63 +27,71 @@
             </div> --}}
             <div class="row">
                 <div class="team3">
-              
-                      <div class="row">
+
+                    <div class="row">
                         @foreach ($foundings as $f)
-                            
-                        <!-- column  -->
-                        <div class="col-lg-4 mb-4">
-                            <!-- Row -->
-                            <div class="item-single ">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <img src="{{url('storage')}}/founding/{{$f->avatar}}" alt="wrapkit" class="img-fluid" />
-                                </div>
-                                <div class="col-md-12">
-                                        <center>
-                                        <h5 class="mt-4 font-weight-medium mb-0">{{$f->name}}</h5>
-                                        <h6 class="subtitle">( {{$f->title}} )</h6>
-                                        </center>
-                                        <div class="ml-4 mr-4 description">
-                                            {!! $f->description !!}
+                            <!-- column  -->
+                            <div class="col-lg-4 mb-4">
+                                <!-- Row -->
+                                <div class="item-single ">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <img src="{{ url('storage') }}/founding/{{ $f->avatar }}" alt="wrapkit"
+                                                class="img-fluid" />
                                         </div>
-                               <br>
-                                        <center><div class="social-link">
-                                            @if(!empty($f->phone))<a href="tel:{{$f->phone}}" target="_blank"><i
-                                                class='bx bxl-phone-call'></i></a>
-                                                @endif
-                                                @if(!empty($f->whatsapp))<a href="https://wa.me/{{$f->whatsapp}}" target="_blank"><i
-                                                    class='bx bxl-whatsapp'></i></a>
+                                        <div class="col-md-12">
+                                            <center>
+                                                <h5 class="mt-4 font-weight-medium mb-0">{{ $f->name }}</h5>
+                                                <h6 class="subtitle">( {{ $f->title }} )</h6>
+                                            </center>
+                                            <div class="ml-4 mr-4 description">
+                                                {!! $f->description !!}
+                                            </div>
+                                            <br>
+                                            <center>
+                                                <div class="social-link">
+                                                    @if (!empty($f->phone))
+                                                        <a href="tel:{{ $f->phone }}" target="_blank"><i
+                                                                class='bx bxs-phone-call'></i></a>
                                                     @endif
-                                            @if(!empty($f->facebook))<a href="{{$f->facebook}}" target="_blank"><i
-                                                    class='bx bxl-facebook'></i></a>
+                                                    @if (!empty($f->whatsapp))
+                                                        <a href="https://wa.me/{{ $f->whatsapp }}" target="_blank"><i
+                                                                class='bx bxl-whatsapp'></i></a>
                                                     @endif
-                                                    @if(!empty($f->instagram))<a href="{{$f->instagram}}" target="_blank"><i
-                                                        class='bx bxl-instagram'></i></a>
-                                                        @endif
-                                                        @if(!empty($f->twitter))<a href="{{$f->twitter}}" target="_blank"><i
-                                                            class='bx bxl-twitter'></i></a>
-                                                            @endif
-                                                            @if(!empty($f->linkedin))<a href="{{$f->linkedin}}" target="_blank"><i
+                                                    @if (!empty($f->facebook))
+                                                        <a href="{{ $f->facebook }}" target="_blank"><i
+                                                                class='bx bxl-facebook'></i></a>
+                                                    @endif
+                                                    @if (!empty($f->instagram))
+                                                        <a href="{{ $f->instagram }}" target="_blank"><i
+                                                                class='bx bxl-instagram'></i></a>
+                                                    @endif
+                                                    @if (!empty($f->twitter))
+                                                        <a href="{{ $f->twitter }}" target="_blank"><i
+                                                                class='bx bxl-twitter'></i></a>
+                                                    @endif
+                                                    @if (!empty($f->linkedin))
+                                                        <a href="{{ $f->linkedin }}" target="_blank"><i
                                                                 class='bx bxl-linkedin'></i></a>
-                                                                @endif
-                                          
+                                                    @endif
+
+
+                                                </div>
+                                            </center>
+                                            <br>
                                         </div>
-                                        </center>
-                                        <br>
+                                    </div>
+
                                 </div>
+                                <!-- Row -->
                             </div>
-                                
-                            </div>
-                            <!-- Row -->
-                        </div>
                         @endforeach
                         <!-- column  -->
                         <!-- column  -->
-                        
+
                         <!-- column  -->
-                      </div>
-                  </div>
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -92,7 +99,7 @@
     <!-- end team section -->
 @endsection()
 <script>
-       document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function() {
         var paragraphs = document.querySelectorAll(".description");
 
         paragraphs.forEach(function(paragraph) {
